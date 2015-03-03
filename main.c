fb4a5804821873c0d51b4efc0dc9d84772602de5
Absurd Noise <absourd.noise@gmail.com>
2015-03-03 16:43:32 +0200

    Test changes

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 
+
 int checkTCPConnetcion(char ip_address[15],int port) {
   printf("Cheking %s:%d\n", ip_address, port);
   return 0;
 }
 
 int main(int argc, char **argv) {
-    checkTCPConnetcion("213.186.120.101", 80);
-    return 0;
+  checkTCPConnetcion("213.186.120.101", 80);
+  return 0;
 }
