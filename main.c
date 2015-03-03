3c1aa758c143a4ae66c3470e5e5b8b6df79b3189
Absurd Noise <absourd.noise@gmail.com>
2015-03-03 16:35:47 +0200

    First Commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int checkTCPConnetcion(char ip_address[15],int port) {
+  printf("Cheking %s:%d\n", ip_address, port);
+  return 0;
+}
+
+int main(int argc, char **argv) {
+    checkTCPConnetcion("213.186.120.101", 80);
+    return 0;
+}
